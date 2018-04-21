c9e856f86ea12b16bf52b1ef93d81004c2ce3095
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-04-21 10:39:17 +0100

    struct

--- src/struct.c
+++ src/struct.c
@@ -9,6 +9,24 @@ typedef struct heap{
     elem *array;
 }*Heap;
 
+typedef struct elemPosts{
+    long id;  /*id do post */
+    Date data; /* Data de um post */
+}elemP;
+
+typedef struct heapPosts{
+    int   size; /* Tamanho alocado para a Heap */
+    int   used; /* Número de elementos da Heap */
+    elemP *array;
+}*HeapPosts;
+
+typedef struct tags{
+	long id; /*id da tag*/
+	char* tagName; /*nome da tag*/
+} Tags;
+
+typedef struct tags **HashTableTags;
+
 typedef struct treeHash{
     long contadorR; /* Número de pots tipo resposta */
     long contadorP; /* Número de posts tipo pergunta */
@@ -17,3 +35,30 @@ typedef struct treeHash{
 } TreeHash;
 
 typedef struct treeHash **TreeHashData;
+
+typedef struct TCD_community{
+	int dataSize;  /* Tamanho da tabela de hash das datas/pots */
+	int usersSize; /* Tamanho da tabela de hash dos utilizadores */
+	int tagsSize; /* Tamanho da tabela de hash das tags */
+	TreeHashData treeHash; /* Tabela de hash com os vários dias/pots */
+	HashTableUsers hashUser;  /* Tabela de hash dos utilizadores */
+	HashTableTags hashTag; /* Tabela de hash das tags */
+	long *topN; /* Apontador para o array com os ids do top N */
+	Heap Top; /* Heap com os ids de utilizador organizada pelo número de posts*/
+}TCD_community;
+
+int TAD_community_get_usersSize(TAD_community com){
+	return com->usersSize;
+}
+
+int TAD_community_get_dataSize(TAD_community com){
+	return com->dataSize;
+}
+
+int TAD_community_get_tagsSize(TAD_community com){
+	return com->tagsSize;
+}
+
+long get_topN(TAD_community com, int i){
+	return(com->topN[i]);
+}
