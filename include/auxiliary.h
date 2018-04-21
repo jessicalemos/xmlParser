61fdd0ea7761137af4655228f3e1690cf7436810
AnaSRibeiro <a_m_s_ribeiro@hotmail.com>
2018-04-21 16:30:02 +0100

    auxiliary

--- include/auxiliary.h
+++ include/auxiliary.h
@@ -11,12 +11,13 @@ float get_fst_new(NEW_pair pair);
 int get_trd_new(NEW_pair pair);
 long get_snd_new(NEW_pair pair);
 void free_new_pair(NEW_pair pair);
+int max(int e,int d);
 int verificaTag(char *s,char *t);
 void insere (int x, long id, long *p, int *s,int N);
 int elimRep (long v[], int N);
 int contida (char* s,char* word);
 int procuraArray (long* postId, long id, int N);
-int compareDate (Date d1, Date d2)
+int compareDate (Date d1, Date d2);
 int compareDateQ (Date d1, Date d2);
 void insereT(Date d,long postId,Date *data,long *id,int N);
 
