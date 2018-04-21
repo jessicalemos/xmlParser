3d689e96801e6bfd81dade7546d2f42329223950
AnaSRibeiro <a_m_s_ribeiro@hotmail.com>
2018-04-21 13:20:30 +0100

    auxiliary

--- include/auxiliary.h
+++ include/auxiliary.h
@@ -1,5 +1,6 @@
 #ifndef __AUXILIARY_H__
 #define __AUXILIARY_H__
+#include "date.h"
 typedef struct new_pair* NEW_pair;
 
 NEW_pair create_new_pair(float fst, long snd, int trd);
@@ -10,10 +11,12 @@ float get_fst_new(NEW_pair pair);
 int get_trd_new(NEW_pair pair);
 long get_snd_new(NEW_pair pair);
 void free_new_pair(NEW_pair pair);
+int verificaTag(char *s,char *t);
 void insere (int x, long id, long *p, int *s,int N);
 int elimRep (long v[], int N);
 int contida (char* s,char* word);
 int procuraArray (long* postId, long id, int N);
+int compareDate (Date d1, Date d2)
 int compareDateQ (Date d1, Date d2);
 void insereT(Date d,long postId,Date *data,long *id,int N);
 
