862abf3c3b378098b6849f6c3f6139ac9b11a436
pedromendespinto <pedrompinto.007@gmail.com>
2018-04-23 18:55:43 +0100

    queries

--- src/queries.c
+++ src/queries.c
@@ -15,14 +15,6 @@ void selectionSort(long* p, int tam){
 	}
 }
 
-int contaTitulo (TAD_community com,char *word){
-	int i, c = 0;
-	for(i=0;i<TAD_community_get_dataSize(com);i++)
-		if(existeTree(com,i))
-			c += retiraTitulo(com,i,word);  
-	return c;
-}
-
 int conta (TAD_community com, int localB, int localE, Date begin, Date end, char* tag){
 	int i, c = 0;
 	for(i=localB;i<=localE;i++){ 
@@ -135,36 +127,29 @@ LONG_list most_answered_questions(TAD_community com, int N, Date begin, Date end
 }
 
 LONG_list contains_word(TAD_community com, char* word, int N){
-	int c = contaTitulo(com,word), j, i;
-	long* id = malloc(c*sizeof(long));
-	Date* data = malloc(c*sizeof(Date));
-	for(i=0;i<c;i++){
+	int j, i;
+	long* id = malloc(N*sizeof(long));
+	Date* data = malloc(N*sizeof(Date));
+	for(i=0;i<N;i++){
 		id[i]=-2;
 		data[i]=NULL;
 	}
 	LONG_list l;
-	if(c!=0){
-		procuraTitulo(com,word,data,id,c);
-		if(c<N) N=c;
-		for(j=0;j<N;j++);
+		procuraTitulo(com,word,data,id,N);
+		for(j=0;j<N && id[j]!=-2;j++);
 		l = create_list(j);
 		for(int k=0;k<j;k++){
 			set_list(l,k,id[k]);
 		}
-	}
-	else l = create_list(0);
-	free(id);
-    free(data);
+	free(id); free(data);
 	return l;
 }
 
 LONG_list questions_with_tag(TAD_community com, char* tag, Date begin, Date end){
-	int chaveB = dataHash(begin, com), chaveE = dataHash(end, com), c = 0, n, l, i, k, j,w; 
+	int c = 0,w; 
 	long *array;
-	LONG_pair local = existeData(com,begin,end,chaveB,chaveE);
-	long localB = get_fst_long(local), localE = get_snd_long(local);
 	if(compareDateQ(begin,end)==2) return create_list(0);
-	if(localB==-1 && localE==-1){
+	else{
 		c = conta(com, 0, TAD_community_get_dataSize(com)-1, begin, end, tag); 
 		array = malloc(c*sizeof(long));
 		for(int j=0; j<c; j++) array[j]=-2;
@@ -176,78 +161,10 @@ LONG_list questions_with_tag(TAD_community com, char* tag, Date begin, Date end)
 			}
 		}
 	}
-	else if(localB!=-1 && localE!=-1 && localB<=localE){ 
-		c = conta(com, localB, localE, begin, end, tag); 
-		array = malloc(c*sizeof(long));
-		for(int j=0; j<c; j++) array[j]=-2; 
-		for(i=localB;i<=localE;i++){
-			if(existeTree(com,i)){
-				if(compareDateQ(post_getCreationDate (com,i), begin)!=0 && compareDateQ(post_getCreationDate (com,i), end)!=2){
-					{retornaId (com, i, array, tag, c); }
-				}	
-			}
-		}
-	}
-	else if(localB==-1 && localE!=-1){
-		 if(localE<chaveB){
-			c = conta(com, chaveB, localE, begin, end, tag);
-			array = malloc(c*sizeof(long));
-			for(int j=0; j<c; j++) array[j]=-2;
-			for(l=chaveB;l!=localE && l<TAD_community_get_dataSize(com);l++){
-				if(existeTree(com,l)){
-					if(compareDateQ(post_getCreationDate (com,l), begin)!=0 && compareDateQ(post_getCreationDate (com,l), end)!=2){
-	       				retornaId (com, l, array, tag, c);
-					}
-				}
-			}
-				for(n=0;n!=localE;n++){
-					if(existeTree(com,n)){
-						if(compareDateQ(post_getCreationDate (com,n), begin)!=0 && compareDateQ(post_getCreationDate (com,n), end)!=2){
-							retornaId (com, n, array, tag, c);
-						}
-					}
-				}
-			retornaId (com, localE, array, tag, c);
-		}
-	    else{
-			c = conta(com, chaveB, localE, begin, end, tag);
-			array = malloc(c*sizeof(long));
-			for(int j=0; j<c; j++) array[j]=-2;
-			for(l=chaveB;l<=localE && l<TAD_community_get_dataSize(com);l++){
-				if(existeTree(com,l)){
-					if(compareDateQ(post_getCreationDate (com,l), begin)!=0 && compareDateQ(post_getCreationDate (com,l), end)!=2){
-	       				retornaId (com, l, array, tag, c);
-					}
-				}
-			}
-		}
-	}
-	else{
-		c = conta(com,0,TAD_community_get_dataSize(com)-1, begin, end, tag);
-		array = malloc(c*sizeof(long));
-		for(int j=0; j<c; j++) array[j]=-2;
-		for(j=localB;j<TAD_community_get_dataSize(com);j++){
-			if(existeTree(com,j)){
-				if(compareDateQ(post_getCreationDate (com,j), begin)!=0 && compareDateQ(post_getCreationDate (com,j), end)!=2){
-					retornaId (com, j, array, tag, c);
-				}
-			}
-		}
-		if(localE==-1) localE=localB-1;
-			for(k=0;k<=localE; k++){
-				if(existeTree(com,k)){
-					if(compareDateQ(post_getCreationDate (com,k), begin)!=0 && compareDateQ(post_getCreationDate (com,k), end)!=2) {
-						retornaId (com, k, array, tag, c);
-					}
-				}
-			}
-		} 
 	LONG_list list = create_list(c);
 	selectionSort(array,c); 
-	for (int i=0; i<c; i++){
+	for (int i=0; i<c; i++)
 		set_list(list, i, array[i]); 
-	}
-	free_long_pair(local);
 	free(array);
 	return list;
 }
