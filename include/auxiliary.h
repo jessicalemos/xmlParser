e19565b39d9ea405d18bf15e99f9f4a1b1833e35
AnaSRibeiro <a_m_s_ribeiro@hotmail.com>
2018-04-26 13:58:10 +0100

    auxiliary

--- include/auxiliary.h
+++ include/auxiliary.h
@@ -1,7 +1,9 @@
 #ifndef __AUXILIARY_H__
 #define __AUXILIARY_H__
 #include "date.h"
+
 typedef struct new_pair* NEW_pair;
+typedef struct query7 **HashTableQuery7;
 typedef struct query11 **HashTableQuery11;
 NEW_pair create_new_pair(float fst, long snd, int trd);
 void set_fst_new(NEW_pair pair, float l);
@@ -20,6 +22,14 @@ int compareDate (Date d1, Date d2);
 int compareDateQ (Date d1, Date d2);
 void insereT(Date d,long postId,Date *data,long *id,int N);
 void insereTag (long tag, long* p, int* s, int N);
+long get_id_Q7(HashTableQuery7 h,int i);
+int get_flag_Q7(HashTableQuery7 h,int i);
+int get_contador_Q7(HashTableQuery7 h,int i);
+void set_id_Q7(HashTableQuery7 h, long l,int i);
+void set_flag_Q7(HashTableQuery7 h, int flag,int i);
+void set_contador_Q7(HashTableQuery7 h, int l,int i);
+HashTableQuery7 initHashQ7 (int N);
+void insereTQ7 (HashTableQuery7 h1, int i,int flag,int contador, long id);
 int existeQ7(HashTableQuery7 h,int i);
 long get_id(HashTableQuery11 h,int i);
 char* get_tag(HashTableQuery11 h,int i);
