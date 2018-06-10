3b1a32d7c7dec4fbd06eecc89913e467f2c9f08d
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-10 21:39:40 +0100

    Post

--- proj-java/src/main/java/engine/Post.java
+++ proj-java/src/main/java/engine/Post.java
@@ -3,6 +3,11 @@ public class Post {
     private long parentID; 
     private long id;
     private String tag;
+    private String title; 
+    private long ownerUserID; 
+    private int answerCount; 
+    private int commentCount;
+    private int score;
 
     public Post() {
         this.postTypeId = 0;
@@ -14,6 +19,18 @@ public class Post {
         this.answerCount = 0;
         this.commentCount = 0;
         this.score = 0;
+    }
+
+    public Post(int postTypeId, long parentID, long id, String tag, String title, long ownerUserID, int answerCount, int commentCount, int score) {
+        this.postTypeId = postTypeId;
+        this.parentID = parentID;
+        this.id = id;
+        this.tag = tag;
+        this.title = title;
+        this.ownerUserID = ownerUserID;
+        this.answerCount = answerCount;
+        this.commentCount = commentCount;
+        this.score = score;
     }
 
     public int getPostTypeId() {
@@ -30,5 +47,78 @@ public class Post {
 
     public String getTag() {
         return tag;
+    }
+
+       public String getTitle() {
+        return title;
+    }
+
+    public long getOwnerUserID() {
+        return ownerUserID;
+    }
+
+    public int getAnswerCount() {
+        return answerCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setPostTypeId(int postTypeId) {
+        this.postTypeId = postTypeId;
+    }
+
+    public void setParentID(long parentID) {
+        this.parentID = parentID;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setOwnerUserID(long ownerUserID) {
+        this.ownerUserID = ownerUserID;
+    }
+
+    public void setAnswerCount(int answerCount) {
+        this.answerCount = answerCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if ((o == null) || (this.getClass() != o.getClass()))
+            return false;
+        Post p = (Post) o;
+        return this.postTypeId == p.getPostTypeId() && p.getParentID() == this.parentID &&
+                p.getId() == (this.id) && p.getTag().equals(this.tag) && p.getTitle().equals(this.title) &&
+                p.getOwnerUserID() == (this.getOwnerUserID()) &&
+                p.getAnswerCount() == (this.answerCount) && p.getCommentCount() == (this.commentCount) &&
+                p.getScore() == (this.score);
+    }
+
+    public Post clone() {
+        return new Post(this);
     }
 }
