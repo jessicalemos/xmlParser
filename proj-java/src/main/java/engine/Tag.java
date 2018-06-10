6beeea3f1e37a1016733b998494d1e060c34d2cd
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-10 22:23:16 +0100

    Tag

--- proj-java/src/main/java/engine/Tag.java
+++ proj-java/src/main/java/engine/Tag.java
@@ -4,8 +4,21 @@ public class Tag {
     private long id;
     private String tagName;
 
+    public Tag(){
+        this.id = 0;
+        this.tagName = "";
+    }
+
     public Tag(Tag t) {
         this.id = t.getId();
         this.tagName = t.getTagName();
     }
 
+	public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if ((o == null) || (this.getClass() != o.getClass()))
+            return false;
+        Tag t = (Tag) o;
+        return t.getId() == (this.id) && t.getTagName().equals(this.tagName);
+    }
