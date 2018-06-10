f77913b6a78ecf8f67a6828d8d51edeb3ba5a324
AnaSRibeiro <a_m_s_ribeiro@hotmail.com>
2018-06-10 21:28:29 +0100

    TreeHash

--- proj-java/src/main/java/engine/TreeHash.java
+++ proj-java/src/main/java/engine/TreeHash.java
@@ -4,12 +4,35 @@ public class TreeHash {
     private long contadorP;
     private LocalDate creationDate;
 
+    public TreeHash() {
+        this.postTree = new TreeMap <Long, Post>();
+        this.contadorR = 0;
+        this.contadorP = 0;
+    }
+
+    public TreeHash(TreeHash a) {
+        this.postTree = a.getPostTree();
+        this.contadorR = a.getContadorR();
+        this.contadorP = a.getContadorP();
+        this.creationDate = a.getCreationDate();
+    }
+
     public Map<Long, Post> getPostTree(){
         Map<Long, Post> neo = new TreeMap<Long, Post>();
         for(Map.Entry<Long, Post> entry : this.postTree.entrySet()){
             neo.put(entry.getKey(), entry.getValue());
         }
         return neo;
+    }
+
+    private void setPostTree(Map<Long, Post> utilizadores) {
+        this.postTree = new TreeMap<Long, Post>();
+        for(Map.Entry<Long, Post> entry: utilizadores.entrySet())
+            this.postTree.put(entry.getKey(), entry.getValue().clone());
+    }
+
+    public void addTreeMap(Post p){
+        this.postTree.put(p.getId(),p);
     }
 
     public boolean equals(Object o) {
