2341c363aee30b32c907e9db92ccc9fb820381ab
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-10 21:13:38 +0100

    maxList

---
+++ proj-java/src/main/java/engine/maxList.java
@@ -0,0 +1,29 @@
+public class maxList {
+    private long id;
+    private LocalDate creationDate;
+
+    public maxList(maxList c) {
+        this.id = c.getId();
+        this.creationDate = c.getCreationDate();
+    }
+	
+	public long getId() {
+        return id;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public maxList clone() {
+        return new maxList(this);
+    }
+}
