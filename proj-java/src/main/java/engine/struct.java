359f57788b4748120fce94554bae59518ec5a9c3
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-10 21:11:53 +0100

    struct

--- proj-java/src/main/java/engine/struct.java
+++ proj-java/src/main/java/engine/struct.java
@@ -4,6 +4,13 @@ public class Struct {
     private Map<Long, Users> userHashTable;
     private List<maxPosts> topN;
 
+    public Struct() {
+        this.tags = new HashMap<Long, Tag>();
+        this.treeHashTable = new HashMap<LocalDate, TreeHash>();
+        this.userHashTable = new HashMap<Long, Users>();
+        this.topN = new ArrayList<maxPosts>();
+    }
+
     public void addPosts(Post p, LocalDate data){
         if (this.treeHashTable.containsKey(data)) {
             this.treeHashTable.get(data).addTreeMap(p.clone());
