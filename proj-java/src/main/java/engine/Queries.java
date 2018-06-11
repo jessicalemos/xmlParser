d31363189653580c1fab94fc7d4679305c085d00
AnaSRibeiro <a_m_s_ribeiro@hotmail.com>
2018-06-11 18:00:17 +0100

    Queries

--- proj-java/src/main/java/engine/Queries.java
+++ proj-java/src/main/java/engine/Queries.java
@@ -15,6 +15,11 @@ public class Queries implements TADCommunity {
     private Struct com;
     private Parse parse;
 
+    public void init(){
+        this.com = new Struct();
+        this.parse = new Parse();
+    }
+
     public void load(String dumpPath){
         init();
         ArrayList<String> aux = new ArrayList<>();
@@ -38,6 +43,27 @@ public class Queries implements TADCommunity {
         this.com.addTop();
     }
 
+    public Pair<String,String> infoFromPost(long id){
+        String nome = null, title = null;
+        Post p = this.com.procuraPost(id);
+        if (p != null){
+            if (p.getPostTypeId() == 1){
+                title = p.getTitle();
+                if(this.com.getUserHashTable().containsKey(p.getOwnerUserID()))
+                    nome = this.com.getUserHashTable().get(p.getOwnerUserID()).getDisplayName();
+            }
+            else{
+                Post parent = this.com.procuraPost(p.getParentID());
+                title = parent.getTitle();
+                if(this.com.getUserHashTable().containsKey(parent.getOwnerUserID()))
+                    nome = this.com.getUserHashTable().get(parent.getOwnerUserID()).getDisplayName();
+            }
+        }
+        Pair pair;
+        pair = new Pair(title,nome);
+        return pair;
+    }
+
     public List<Long> topMostActive(int N){
         List<Long> aux = new ArrayList<Long>();
         if(N<=0) return aux;
@@ -47,10 +73,37 @@ public class Queries implements TADCommunity {
         return aux;
     }
 
+    public Pair<Long,Long> totalPosts(LocalDate begin, LocalDate end){
+        long respostas = 0, perguntas = 0;
+        Pair pair;
+        pair = new Pair(perguntas,respostas);
+        if(end.isBefore(begin)) return pair;
+        this.com.getPair(pair,begin,end);
+        return pair;
+    }
+
     public List<Long> questionsWithTag(String tag, LocalDate begin, LocalDate end){
         List<Long> aux = new ArrayList<Long>();
         if(end.isBefore(begin)) return aux;
         this.com.getList(aux,begin,end,tag);
+        return aux;
+    }
+
+    public Pair<String, List<Long>> getUserInfo(long id){
+        String aboutMe = null;
+        List<Long> ids = new ArrayList<Long>();
+        if(this.com.getUserHashTable().containsKey(id)){
+            aboutMe = this.com.getUserHashTable().get(id).getAboutMe();}
+        this.com.getInfo(ids,id);
+        Pair pair;
+        pair = new Pair(aboutMe,ids);
+        return pair;
+    }
+
+    public List<Long> mostVotedAnswers(int N, LocalDate begin, LocalDate end){
+        List<Long> aux = new ArrayList<Long>();
+        if(end.isBefore(begin) || N<=0) return aux;
+        this.com.mostVoted(aux,begin,end,N);
         return aux;
     }
 
@@ -68,6 +121,13 @@ public class Queries implements TADCommunity {
         return aux;
     }
 
+    public List<Long> bothParticipated(int N, long id1, long id2){
+        List<Long> aux = new ArrayList<Long>();
+        if(N<=0) return aux;
+        this.com.both(aux,N,id1,id2);
+        return aux;
+    }
+    
     public long betterAnswer(long id){
         return this.com.answer(id);
     }
