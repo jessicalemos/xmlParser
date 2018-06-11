920d3be43a0a49861feaaa9e301f1bad77adca73
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-11 17:50:27 +0100

    Queries

--- proj-java/src/main/java/engine/Queries.java
+++ proj-java/src/main/java/engine/Queries.java
@@ -15,6 +15,45 @@ public class Queries implements TADCommunity {
     private Struct com;
     private Parse parse;
 
+    public void load(String dumpPath){
+        init();
+        ArrayList<String> aux = new ArrayList<>();
+        StringBuilder su = new StringBuilder();
+        su.append(dumpPath);
+        su.append("/Users.xml");
+        aux.add(su.toString());
+        StringBuilder sp = new StringBuilder();
+        sp.append(dumpPath);
+        sp.append("/Posts.xml");
+        aux.add(sp.toString());
+        StringBuilder st = new StringBuilder();
+        st.append(dumpPath);
+        st.append("/Tags.xml");
+        aux.add(st.toString());
+        for(int i=0; i<3; i++){
+            if (i==0) parse.parseUsers(aux.get(i),this.com);
+            else if (i==1) parse.parsePosts(aux.get(i),this.com);
+            else parse.parseTags(aux.get(i),this.com);
+        }
+        this.com.addTop();
+    }
+
+    public List<Long> topMostActive(int N){
+        List<Long> aux = new ArrayList<Long>();
+        if(N<=0) return aux;
+        List<maxPosts> top = this.com.getTopN();
+        for(int i=0; i<N && i<top.size(); i++)
+            aux.add(top.get(i).getId());
+        return aux;
+    }
+
+    public List<Long> questionsWithTag(String tag, LocalDate begin, LocalDate end){
+        List<Long> aux = new ArrayList<Long>();
+        if(end.isBefore(begin)) return aux;
+        this.com.getList(aux,begin,end,tag);
+        return aux;
+    }
+
     public List<Long> mostAnsweredQuestions(int N, LocalDate begin, LocalDate end){
         List<Long> aux = new ArrayList<Long>();
         if(end.isBefore(begin) || N<=0) return aux;
@@ -22,10 +61,30 @@ public class Queries implements TADCommunity {
         return aux;
     }
 
+    public List<Long> containsWord(int N, String word){
+        List<Long> aux = new ArrayList<Long>();
+        if(N<=0) return aux;
+        this.com.containsW(aux,word,N);
+        return aux;
+    }
+
+    public long betterAnswer(long id){
+        return this.com.answer(id);
+    }
+
     public List<Long> mostUsedBestRep(int N, LocalDate begin, LocalDate end){
         List<Long> aux = new ArrayList<Long>();
         if(end.isBefore(begin) || N<=0) return aux;
         this.com.bestRep(aux,N,begin,end);
         return aux;
+    }
+
+    public void clear(){
+        this.com.getUserHashTable().clear();
+        this.com.getTreeHashTable().clear();
+        this.com.getTopN().clear();
+        this.com.getTopR().clear();
+        this.com.getTags().clear();
+        this.com = null;
     }
 }
