14950451acd9da41c2d753cc78f5ef292a13faf9
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-11 19:16:42 +0100

    maxMapComparator

--- proj-java/src/main/java/engine/maxMapComparator.java
+++ proj-java/src/main/java/engine/maxMapComparator.java
@@ -3,6 +3,13 @@ package src.main.java.engine;
 import java.util.Comparator;
 
 public class maxMapComparator implements Comparator<maxMap> {
+
+    /**
+     * Compara duas maxMaps consoante o contador
+     * @param c1 maxMap 1
+     * @param c2 maxMap 2
+     * @return O contador da maxMap 1 é maior (-1); O contador da maxMap 2 é maior (1); São iguais (0);
+     */
     public int compare(maxMap c1, maxMap c2) {
         if (c1.getCount() < c2.getCount()) return 1;
         if (c1.getCount() > c2.getCount()) return -1;
