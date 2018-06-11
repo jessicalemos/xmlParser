da22b67f150a9350c982e3515f2652fc9cb75bba
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-11 17:06:56 +0100

    struct

--- proj-java/src/main/java/engine/struct.java
+++ proj-java/src/main/java/engine/struct.java
@@ -69,4 +69,97 @@ public class Struct {
         }
         Collections.sort(this.topN, new nPostsComparator());
     }
+
+    public void getPair(Pair p, LocalDate begin, LocalDate end){
+        long respostas = 0, perguntas = 0;
+        for(TreeHash t : this.treeHashTable.values())
+            if(t.getCreationDate().equals(begin) || t.getCreationDate().equals(end) ||
+                    (t.getCreationDate().isAfter(begin) && t.getCreationDate().isBefore(end))){
+                respostas += t.getContadorR();
+                perguntas += t.getContadorP();
+            }
+        p.setFst(perguntas);
+        p.setSecond(respostas);
+    }
+
+    public void both(List<Long> aux, int N, long id1, long id2){
+        if(this.userHashTable.containsKey(id1) && this.userHashTable.containsKey(id2)) {
+            List<maxList> l1 = this.userHashTable.get(id1).getUserList();
+            List<maxList> l2 = this.userHashTable.get(id2).getUserList();
+            List<Long> parents = new ArrayList<Long>();
+            List<maxList> l = new ArrayList<maxList>();
+            List<Long> v = new ArrayList<Long>();
+            List<maxList> lmaior = l2, lmenor = l1;
+            if(l1.size() > l2.size()) {
+                lmaior = l1;
+                lmenor = l2;
+            }
+            for(maxList m : lmenor){
+                Post p = this.treeHashTable.get(m.getCreationDate()).getPostTree().get(m.getId());
+                if(p.getPostTypeId() == 2){
+                    long parent = p.getParentID();
+                    parents.add(parent);
+                }
+                else parents.add(p.getId());
+            }
+            for(maxList i : lmaior) {
+                Post p = this.treeHashTable.get(i.getCreationDate()).getPostTree().get(i.getId());
+                if (p.getPostTypeId() == 2) {
+
+    public long answer(long id) {
+        int nRespostas = 0;
+        double c = 0, max = 0;
+        long idMax = -1;
+        Post p = procuraPost(id);
+        if (p != null) nRespostas = p.getAnswerCount();
+        for (TreeHash i : this.treeHashTable.values()) {
+            for (Post j : i.getPostTree().values())
+                if (j.getPostTypeId() == 2 && j.getParentID() == id) {
+                    c = 0.25 * this.userHashTable.get(j.getOwnerUserID()).getReputation() + j.getScore() * 0.65 +
+                            j.getCommentCount() * 0.1;
+                    if (c > max) {
+                        max = c;
+                        idMax = j.getId();
+                    }
+                    nRespostas--;
+                }
+            if (nRespostas == 0) break;
+        }
+        return idMax;
+    }
+
+    public void bestRep(List<Long> aux, int N, LocalDate begin, LocalDate end ){
+        List<maxPosts> m = new ArrayList<maxPosts>();
+        List<Long> users = new ArrayList<Long>();
+        Map<Long,maxPosts> tag = new HashMap<Long,maxPosts>();
+        for(int i=0; i<N && i<this.topR.size(); i++) {
+            users.add(this.topR.get(i).getId());
+        }
+        for (TreeHash t : this.treeHashTable.values())
+            if (t.getCreationDate().equals(begin) || t.getCreationDate().equals(end) ||
+                    (t.getCreationDate().isAfter(begin) && t.getCreationDate().isBefore(end))) {
+                for (Post p : t.getPostTree().values()) {
+                       if(users.contains(p.getOwnerUserID()) && p.getTag()!=null){
+                           String[] splitted = p.getTag().split(">");
+                            for (int i = 0; i< splitted.length; i++){
+                                String[] splitted2 = splitted[i].split("<");
+                                if(splitted2.length>1) {
+                                    long tagId = this.tags.get(splitted2[1]).getId();
+                                    if(!tag.containsKey(tagId)) {
+                                        maxPosts mx = new maxPosts(tagId, 1);
+                                        tag.put(tagId, mx);
+                                    }
+                                    else tag.get(tagId).setnPosts(tag.get(tagId).getNPosts()+1);
+                                }
+                        }
+                    }
+                }
+         }
+         List<maxPosts> max = new ArrayList<maxPosts>();
+         for(maxPosts j : tag.values())
+             max.add(j.clone());
+         Collections.sort(max,new maxPostsComparator());
+         for(int i=0; i<N && i<max.size(); i++)
+             aux.add(max.get(i).getId());
+    }
 }
