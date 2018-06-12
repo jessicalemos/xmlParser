df1d8b2c9af2335789cc7a1efa91dd775d75c249
AnaSRibeiro <a_m_s_ribeiro@hotmail.com>
2018-06-12 20:06:40 +0100

    maxMap

--- proj-java/src/main/java/engine/maxMap.java
+++ proj-java/src/main/java/engine/maxMap.java
@@ -11,6 +11,12 @@ public class maxMap {
         this.flag = 0;
     }
 
+    public maxMap(long id, int count, int flag) {
+        this.id = id;
+        this.count = count;
+        this.flag = flag;
+    }
+
     public maxMap(maxMap c) {
         this.id = c.getId();
         this.count = c.getCount();
@@ -29,11 +35,22 @@ public class maxMap {
         return flag;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
     public boolean equals(Object o) {
         if (this == o)
             return true;
         if ((o == null) || (this.getClass() != o.getClass()))
-
+            return false;
+        maxMap p = (maxMap) o;
+        return p.getId() == this.id &&  p.getCount() == this.count && p.getFlag() == this.getFlag();
+    }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -42,6 +59,10 @@ public class maxMap {
         sb.append("com contador ");
         sb.append(this.count);
         sb.append("e flag");
+        sb.append(this.flag);
+        sb.append("\n")
+        return sb.toString();
+    }
 
     public maxMap clone() {
         return new maxMap(this);
