df840fd6ad342c901bb9d8c51b905a13b51f4c89
Jéssica Lemos <jessica.andreia96@gmail.com>
2018-06-12 19:49:58 +0100

    maxMap

--- proj-java/src/main/java/engine/maxMap.java
+++ proj-java/src/main/java/engine/maxMap.java
@@ -5,6 +5,12 @@ public class maxMap {
     private int count;
     private int flag;
 
+    public maxMap() {
+        this.id = 0;
+        this.count = 0;
+        this.flag = 0;
+    }
+
     public maxMap(maxMap c) {
         this.id = c.getId();
         this.count = c.getCount();
@@ -14,6 +20,28 @@ public class maxMap {
     public long getId() {
         return id;
     }
+
+    public int getCount(){ 
+        return count;
+    }
+
+    public int getFlag(){ 
+        return flag;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if ((o == null) || (this.getClass() != o.getClass()))
+
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("O seu id é: ");
+        sb.append(this.id);
+        sb.append("com contador ");
+        sb.append(this.count);
+        sb.append("e flag");
 
     public maxMap clone() {
         return new maxMap(this);
